69ddcebda717bf06c8293cc57ee49931591f3d0b
Fekhreddine23 <fekhredine1990@gmail.com>
2022-10-06 15:36:01 +0200

    init

---
+++ src/diginamic/Bonjour.java
@@ -0,0 +1,16 @@
+package diginamic;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bonjour a tous");
+		
+		int a = 053;
+		
+		System.out.println(a);
+		
+
+	}
+
+}
